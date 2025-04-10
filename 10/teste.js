f7786cec9aed2a1acdbb131e4b1cf1bb9469cb1d
hebert <heberte.2004@gmail.com>
2025-04-09 22:03:25 -0300

    projeto :funcionario

--- 10/teste.js
+++ 10/teste.js
@@ -11,35 +11,50 @@ class Funcionario {
         this._endereco = endereco;
     }
 
-    reberSalario(){
-
+    receberSalario(valor){
+        return `${this._nome} recebeu R$ ${valor}`;
     }
 
-    TempoEmpresa(){
-
+    TempoEmpresa(anoAtual){
+        return anoAtual - this.anoAdimissao;
     }
 
     baterponto(){
-
+        return `Ponto registrado para ${this._nome}`
     }
 }
 
-class Gerente {
+class Gerente extends Funcionario{
     constructor(nome, matricula, anoDeAdimissao ,endereco){
     super(nome, matricula, anoDeAdimissao ,endereco);
     }
-}
-
-class OperadorDeCaixa {
-    constructor(nome, matricula, anoDeAdimissao ,endereco){
-    super(nome, matricula, anoDeAdimissao ,endereco);
+    receberSalario(valor) {
+        let total = valor + (valor * 0.2);
+        return `${this.getNome()} recebu um bonus de R$${total}}`;
+    }
+    autorizarDespesa(valor) {
+        if(valor > 1000) {
+            return `Despa de R$ ${valor} autorizada por ${this.getNome()}`;
+        } else {
+            return `Despa de R$ ${valor} não precisa de autorização ou codigo invalido`;
+        }
     }
 }
 
-class Estagiario {
+class OperadorDeCaixa extends Funcionario{
+    constructor(nome, matricula, anoDeAdimissao ,endereco){
+    super(nome, matricula, anoDeAdimissao ,endereco);
+    }
+    fecharCaixa(valortotal) { 
+        return `${this.getNome()} fechou o caixa com o valor de  R$${valorTotal}.`;
+    }
+}
+
+class Estagiario extends Funcionario{
     constructor (nome, matricula, anoDeAdimissao ,endereco){
         super(nome, matricula, anoDeAdimissao ,endereco)
     }
+
 }
 
 let funcionario = new Funcionario("João Silva", "12345", 2015, "Rua A, 123");
